d830bcc879c3c98ac81269517c445ff2fa41bbf0
Mohammad Marwan Sultan <mohammad.marwan.sultan@gmail.com>
2022-03-11 15:34:44 +0200

    Removed task 2 main method

--- src/csen1002/main/task2/NFA.java
+++ src/csen1002/main/task2/NFA.java
@@ -223,10 +223,5 @@ public class NFA {
 		}
 		return nextNFAStates;
 	}
-	
-	public static void main(String[] args) {
-		NFA nfa = new NFA("2,3#4,5;7,8#0,1;0,7;1,2;1,4;3,6;5,6;6,1;6,7#8");
-		System.out.println(nfa.run(""));
-	}
 
 }
