af2b5b464381aaeb3d82cb42e92a84b364104144
Mohammad Marwan Sultan <mohammad.marwan.sultan@gmail.com>
2022-05-19 01:17:24 +0200

    Modified task 8 public tests

--- src/csen1002/tests/task8/Task8PublicTest.java
+++ src/csen1002/tests/task8/Task8PublicTest.java
@@ -1,14 +1,12 @@
 package csen1002.tests.task8;
 
+import csen1002.main.task8.task8Lexer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.Vocabulary;
-import org.junit.Assert;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import java.util.List;
-
-import csen1002.main.task8.task8Lexer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task8PublicTest {
 
@@ -26,48 +24,38 @@ public class Task8PublicTest {
 		return stringBuilder.toString();
 	}
 
-
-	@Test(timeout=100)
-	public void testFDFA1T0() {
-		Assert.assertEquals("011100,Q2;", tokenStream("011100"));
-	}
-	@Test(timeout=100)
+    @Test
+	public void testFDFA1T0() {assertEquals("011100,Q2;", tokenStream("011100"));}
+	@Test
 	public void testFDFA1T1() {
-		Assert.assertEquals("10100111010,Q3;", tokenStream("10100111010"));
+		assertEquals("10100111010,Q3;", tokenStream("10100111010"));
 	}
-	@Test(timeout=100)
-	public void testFDFA1T2() {
-		Assert.assertEquals("11011001111101,Q4;", tokenStream("11011001111101"));
-	}
-	@Test(timeout=100)
+	@Test
+	public void testFDFA1T2() {assertEquals("11011001111101,Q4;", tokenStream("11011001111101"));}
+	@Test
 	public void testFDFA1T3() {
-		Assert.assertEquals("0001001100,Q2;", tokenStream("0001001100"));
+		assertEquals("0001001100,Q2;", tokenStream("0001001100"));
 	}
-	@Test(timeout=100)
+	@Test
 	public void testFDFA1T4() {
-		Assert.assertEquals("10011,Q4;", tokenStream("10011"));
+		assertEquals("10011,Q4;", tokenStream("10011"));
 	}
-	@Test(timeout=100)
+	@Test
 	public void testFDFA1T5() {
-		Assert.assertEquals("000,Q3;00111,Q4;", tokenStream("00000111"));
+		assertEquals("000,Q3;00111,Q4;", tokenStream("00000111"));
 	}
-	@Test(timeout=100)
+	@Test
 	public void testFDFA1T6() {
-		Assert.assertEquals("0000001,Q4;", tokenStream("0000001"));
+		assertEquals("0000001,Q4;", tokenStream("0000001"));
 	}
-	@Test(timeout=100)
+	@Test
 	public void testFDFA1T7() {
-		Assert.assertEquals("0111100111011101110,Q3;", tokenStream("0111100111011101110"));
+		assertEquals("0111100111011101110,Q3;", tokenStream("0111100111011101110"));
 	}
-	@Test(timeout=100)
-	public void testFDFA1T8() {
-		Assert.assertEquals("0011000001110,Q3;", tokenStream("0011000001110"));
+	@Test
+	public void testFDFA1T8() {assertEquals("0011000001110,Q3;", tokenStream("0011000001110"));}
+	@Test
+	public void testFDFA1T9() {assertEquals("010101011000,Q3;", tokenStream("010101011000"));
 	}
-	@Test(timeout=100)
-	public void testFDFA1T9() {
-		Assert.assertEquals("010101011000,Q3;", tokenStream("010101011000"));
-	}
-
-
 
 }
